01442df1a4ff396592763b1480fc36dd0c28fd3c
Emanuel Fontanha <emanuellira@id.uff.br>
2025-10-27 22:00:12 -0300

    atributo Conversao(nome_moeda) não é mais unique

--- DDL.sql
+++ DDL.sql
@@ -6,7 +6,7 @@ CREATE TABLE Empresa (
 
 CREATE TABLE Conversao (
     sigla_moeda VARCHAR(5) PRIMARY KEY,
-    nome_moeda VARCHAR(20) UNIQUE NOT NULL,
+    nome_moeda VARCHAR(20) NOT NULL,
     fator_conver_to_dolar NUMERIC(10,4) NOT NULL
 );
 
