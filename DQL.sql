4b8c7d143a0d24120095615d13a7856fdcfb3b94
Matheus Campos <matheuscampos14@hotmail.com>
2025-11-12 20:33:04 -0300

    Update DQL.sql

--- DQL.sql
+++ DQL.sql
@@ -11,3 +11,24 @@ JOIN Canal c
     ON p.nome_canal = c.nome 
    AND p.id_plataforma = c.id_plataforma
 ORDER BY e.nome_empresa, c.nome;
+
+-- Terceira Consulta
+SELECT 
+    D.nome_canal,
+    D.id_plataforma,
+    C.tipo,
+    C.nick_streamer,
+    SUM(D.valor) AS total_recebido
+FROM 
+    Doacao D
+    JOIN Canal C 
+        ON D.nome_canal = C.nome
+        AND D.id_plataforma = C.id_plataforma
+WHERE 
+    D.status_doacao = 'recebido'
+GROUP BY 
+    D.nome_canal, D.id_plataforma, C.tipo, C.nick_streamer
+HAVING 
+    SUM(D.valor) > 0
+ORDER BY 
+    total_recebido DESC;
