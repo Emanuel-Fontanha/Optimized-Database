66c92da926f93a7f03b037f31a86057cdde1f1f5
Matheus Campos <matheuscampos14@hotmail.com>
2025-11-12 20:37:39 -0300

    Refactor SQL queries and add a second query

--- DQL.sql
+++ DQL.sql
@@ -12,6 +12,33 @@ JOIN Canal c
    AND p.id_plataforma = c.id_plataforma
 ORDER BY e.nome_empresa, c.nome;
 
+-- Segunda Consulta
+SELECT 
+    U.nick AS usuario,
+    COUNT(DISTINCT I.nome_canal) AS qtd_canais_membro,
+    DATE_TRUNC('month', V.data_hora) AS mes_referencia,
+    SUM(D.valor) AS total_desembolsado
+FROM 
+    Usuario U
+    LEFT JOIN Inscricao I 
+        ON U.nick = I.nick_membro
+    LEFT JOIN Doacao D 
+        ON U.nick = D.nick_usuario
+    LEFT JOIN Video V 
+        ON D.nome_canal = V.nome_canal
+        AND D.id_plataforma = V.id_plataforma
+        AND D.titulo_video = V.titulo
+        AND D.data_hora_vid = V.data_hora
+WHERE 
+    D.status_doacao = 'recebido'
+GROUP BY 
+    U.nick,
+    DATE_TRUNC('month', V.data_hora)
+ORDER BY 
+    U.nick,
+    mes_referencia;
+
+
 -- Terceira Consulta
 SELECT 
     D.nome_canal,
@@ -32,3 +59,4 @@ HAVING
     SUM(D.valor) > 0
 ORDER BY 
     total_recebido DESC;
+
